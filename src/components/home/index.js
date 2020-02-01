ba6502cf75cecccef2bf5997a3b7d9a8d13f1bfe
Midstackdev <midstackdev@gmail.com>
2020-02-01 21:37:46 +0000

    Adding slider

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import NewsSlider from '../widgets/newsSlider'
+
 const Home = () => {
     return (
         <div>
-            Home
+            <NewsSlider 
+                type="featured"
+                start={0}
+                amount={6}
+                settings={{
+                    dots: false
+                }}
+            />
         </div>
     )
 }
---
+++ src/components/widgets/newsSlider/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import SliderTemplate from './sliderTemplate'
+
+class NewsSlider extends Component {
+    state = {
+        news: []
+    }
+
+    componentWillMount() {
+        axios.get(`http://localhost:3005/articles?_start=${this.props.start}&_end=${this.props.amount}`)
+        .then((response) => {
+            this.setState({
+                news: response.data
+            })
+        })
+    }
+
+    render () {
+        // console.log(this.state.news)
+        return (
+            <div>
+                <SliderTemplate data={this.state.news} type={this.props.type} settings={this.props.settings}/>
+            </div>
+        )
+    }
+}
+
+export default NewsSlider
