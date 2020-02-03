ec1dc7b564f3fcecc4411baeebab13e7b38d59df
Midstackdev <midstackdev@gmail.com>
2020-02-03 13:53:58 +0000

    Card info

--- src/components/widgets/newsList/index.js
+++ src/components/widgets/newsList/index.js
@@ -6,10 +6,13 @@ import { URL } from '../../../config'
 import style from './newslist.module.css'
 
 import Button from '../buttons'
+import cardInfo from '../cardInfo'
+import CardInfo from '../cardInfo'
 
 class NewsList extends Component {
     
     state = {
+        teams: [],
         items: [],
         start: this.props.start,
         amount: this.props.amount,
@@ -21,6 +24,15 @@ class NewsList extends Component {
     }
 
     request = (start, end) => {
+        if (this.state.teams.length < 1) {
+            axios.get(`${URL}/teams`)
+            .then((response) => {
+                this.setState({
+                    teams: response.data
+                })
+            })
+        }
+
         axios.get(`${URL}/articles?_start=${start}&_end=${end}`)
         .then((response) => {
             this.setState({
@@ -51,6 +63,11 @@ class NewsList extends Component {
                         <div key={i}>
                             <div className={style.news_list_item}>
                                 <Link to={`/article/${item.id}`}>
+                                    <CardInfo 
+                                        teams={this.state.teams}
+                                        team={item.team}
+                                        date={item.date}
+                                    />
                                     <h2>{item.title}</h2>
                                 </Link>
                             </div>
@@ -68,7 +85,7 @@ class NewsList extends Component {
     
 
     render () {
-        
+
         return (
             <div>
                 <TransitionGroup
