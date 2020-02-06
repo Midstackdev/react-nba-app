cd9d8acf833000220e48f95878ff383e43cabcb6
Midstackdev <midstackdev@gmail.com>
2020-02-06 19:47:09 +0000

    Adding firebase and the slider

--- src/config.js
+++ src/config.js
@@ -2,7 +2,19 @@ const CURRENT_YEAR = (new Date(0)).getFullYear()
 
 const URL = ' http://localhost:3005'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCgTXa0K_2_Zy8IvYmFadcrSlbWQI7Hajo",
+    authDomain: "nba-app-2b751.firebaseapp.com",
+    databaseURL: "https://nba-app-2b751.firebaseio.com",
+    projectId: "nba-app-2b751",
+    storageBucket: "nba-app-2b751.appspot.com",
+    messagingSenderId: "765201721995",
+    appId: "1:765201721995:web:fbf3133e8c21d451eb028e"
+};
+
 export {
     CURRENT_YEAR,
-    URL
+    URL,
+    firebaseConfig
 }
+
